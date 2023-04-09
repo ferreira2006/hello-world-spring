938b4dc20def110267c2573d02b73df5361a64d6
ferreira2006 <ferreira2006@gmail.com>
2023-04-09 09:26:08 -0300

    Pequena alteração

--- helloWorld/src/main/java/com/estudos/helloWorld/HelloWorldApplication.java
+++ helloWorld/src/main/java/com/estudos/helloWorld/HelloWorldApplication.java
@@ -12,6 +12,7 @@ public class HelloWorldApplication {
     public static void main(String[] args) {
       SpringApplication.run(HelloWorldApplication.class, args);
     }
+    
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
       return String.format("Hello %s!", name);
